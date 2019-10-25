c6fa057b1f6b5f52ab5c7756198d19599983a761
datong <hello@772778432@qq.com>
2019-10-25 09:12:08 +0800

    解决pom里面spring-cloud-starter-feign版本问题

---
+++ xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequset.java
@@ -0,0 +1,21 @@
+package com.xuecheng.framework.domain.cms.request;
+
+import lombok.Data;
+
+/**
+ * 方便日久扩展用模型来展示
+ */
+@Data
+public class QueryPageRequset {
+    //接收页面查询的条件
+    //站点ID
+    private String siteId;
+    //页面id
+    private String pageId;
+    //页面名称
+    private String pageName;
+    //别名
+    private String pageAliase;
+    //模板id
+    private String templateId;
+}
